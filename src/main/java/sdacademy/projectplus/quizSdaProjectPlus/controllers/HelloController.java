c52975a9212a84231ba0738c8bd6d92f9b50d225
MihalyMatyas <mihamatyas@gmail.com>
2022-04-29 14:01:08 +0300

    added Bootstrap template and style

--- src/main/java/sdacademy/projectplus/quizSdaProjectPlus/controllers/HelloController.java
+++ src/main/java/sdacademy/projectplus/quizSdaProjectPlus/controllers/HelloController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String hello(Model model){
-        model.addAttribute("message", "some message");
-
-        return "hello";
+        return "index";
     }
 }
